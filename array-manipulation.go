71e0f381970a17b1330b08a9dd88b192dacf3b92
John McKee <jmckee3@mac.com>
2019-11-24 20:22:43 -0800

    working version

--- array-manipulation.go
+++ array-manipulation.go
@@ -1,3 +1,102 @@
-package manipulation
+package main
 
-func main() {}
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func arrayManipulation(n int32, queries [][]int32) int64 {
+	totalArr := make([]int64, n+1)
+	var total int64
+	var tempTotal int64
+
+	for _, arr := range queries {
+		totalArr[arr[0]-1] += int64(arr[2])
+		totalArr[arr[1]] += int64(-arr[2])
+	}
+
+	for _, value := range totalArr {
+		tempTotal += value
+		if tempTotal > total {
+			total = tempTotal
+		}
+	}
+
+	return total
+}
+
+func main() {
+	file, err := os.Open("test-case-4")
+	checkError(err)
+
+	reader := bufio.NewReaderSize(file, 1024*1024)
+
+	stdout, err := os.Create("OUTPUT-test-case-4")
+	checkError(err)
+
+	defer stdout.Close()
+
+	writer := bufio.NewWriterSize(stdout, 1024*1024)
+
+	nm := strings.Split(readLine(reader), " ")
+
+	nTemp, err := strconv.ParseInt(nm[0], 10, 64)
+	checkError(err)
+	n := int32(nTemp)
+
+	mTemp, err := strconv.ParseInt(nm[1], 10, 64)
+	checkError(err)
+	m := int32(mTemp)
+
+	var queries [][]int32
+	for i := 0; i < int(m); i++ {
+		queriesRowTemp := strings.Split(readLine(reader), " ")
+
+		var queriesRow []int32
+		for _, queriesRowItem := range queriesRowTemp {
+			queriesItemTemp, err := strconv.ParseInt(queriesRowItem, 10, 64)
+			checkError(err)
+			queriesItem := int32(queriesItemTemp)
+			queriesRow = append(queriesRow, queriesItem)
+		}
+
+		if len(queriesRow) != int(3) {
+			panic("Bad input")
+		}
+
+		queries = append(queries, queriesRow)
+	}
+
+	result := arrayManipulation(n, queries)
+
+	fmt.Fprintf(writer, "%d\n", result)
+
+	writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+// 100 0   -100 0 0
+// 100 100 -100 0 0
+// 100 100 0    0 -100
+
+// 100 100 0   0   0
+// 100 200 100 100 100
+// 100 200 200 200 100
--- spec-array-manipulation_test.go
+++ spec-array-manipulation_test.go
@@ -1,7 +1,22 @@
-package manipulation
+package main
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
+// expected output is 200
 func TestArrayManipulation1(t *testing.T) {
+	queries := [][]int32{
+		[]int32{1, 2, 100},
+		[]int32{2, 5, 100},
+		[]int32{3, 4, 100},
+	}
+	n := int32(5)
+	total := arrayManipulation(n, queries)
 
+	if total != 200 {
+		t.Errorf("got %d instead of 200", total)
+	}
+	fmt.Println("total:", total)
 }
